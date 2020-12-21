1713c04bedfe36b7c0f317b0f2f9b36958404774
JulianArbini97 <1890@holbertonschool.com>
2020-12-21 21:13:42 +0000

    vuelta

--- push.c
+++ push.c
@@ -3,47 +3,34 @@ var_1 vari;
 
 void push(stack_t **stack, unsigned int line_number)
 {
-    stack_t *new;
-    char *num = vari.num;
-    unsigned int j = 0;
+	stack_t *new;
+	char *num = vari.num;
 
-    (void) line_number;
-    new = malloc(sizeof(stack_t));
-    if (new == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        f_list(vari.mystack);
-        fclose(vari.fil);
-        exit(EXIT_FAILURE);
-    }
 
-    for (j = 0; j < strlen(vari.num); j++)
-    {
-    if (num[j] >= 48 && num[j] <= 57)
-    {
-    new->n = atoi(num);
-    new->next = (*stack);
-    new->prev = NULL;
-    }
-    else
-    {
-	    fprintf(stderr,"L%d: usage: push integer\n", vari.line_number);
-	    fclose(vari.fil);
-	    f_list(vari.mystack);
-	    free(new);
-	    exit(EXIT_FAILURE);
-    }
-    }
-    (*stack) = new;
-    if ((*stack) != NULL)
-    {
-        (*stack)->prev = new;
-    }
-    else
-    {
-        fprintf(stderr,"L%d: usage: push integer\n", vari.line_number);
-                 fclose(vari.fil);
-		 f_list(vari.mystack);
-		 exit(EXIT_FAILURE);
-    }
+	(void) line_number;
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		f_list(vari.mystack);
+		fclose(vari.fil);
+		exit(EXIT_FAILURE);
+	}
+
+	if (*num >= 48 && *num <= 57)
+	{
+		new->n = atoi(num);
+		new->next = (*stack);
+		new->prev = NULL;
+		if ((*stack) != NULL)
+			(*stack)->prev = new;
+		(*stack) = new;
+	}
+	else
+	{
+		fprintf(stderr,"L%d: usage: push integer\n", vari.line_number);
+		fclose(vari.fil);
+		f_list(vari.mystack);
+		exit(EXIT_FAILURE);
+	}
 }
--- test.m
+++ test.m
@@ -1,9 +1,8 @@
 push 0 Push 0 onto the stack
 push 1 Push 1 onto the stack
-2 push
-  push 3
- pall    
-
+push 2334
+ push 3
+pall 456
 push 4
 
     push 5    
