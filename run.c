2ea2454126dd876a7a1a4a069c411158b1c0a977
JulianArbini97 <1890@holbertonschool.com>
2020-12-21 20:15:26 +0000

    monty

--- run.c
+++ run.c
@@ -11,7 +11,6 @@ var_1 vari;
 
 void run(char (*refer)[100])
 {
-	unsigned int i = 0;
 	int j = 0;
      instruction_t funcs[] = {
          {"push", push},
@@ -27,18 +26,6 @@ void run(char (*refer)[100])
          if(strcmp(funcs[j].opcode, refer[0]) == 0)
          {
              vari.num = refer[1];
-
-	     for (i = 0; i < strlen(vari.num); i++)
-	     {
-		     if (vari.num[i] < 47 || vari.num[i] > 58)
-		     {
-			     fprintf(stderr,"L%d: usage: push integer\n", vari.line_number);
-			     fclose(vari.fil);
-			     f_list(vari.mystack);
-			     exit(EXIT_FAILURE);
-		     }
-	     }
-
              funcs[j].f(&(vari.mystack), vari.line_number);
              return;
          }
